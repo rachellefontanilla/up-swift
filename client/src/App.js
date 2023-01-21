1757286a949fef87638c2f6164494d9ab66a0d2d
Rachelle Fontanilla <rachfontanilla@gmail.com>
2023-01-21 17:12:19 -0500

    displaying some user data from Spotify API

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { accessToken, logout } from "./util/spotify";
+import { accessToken, getCurrentUserProfile, logout } from "./spotify/spotify";
 
 function App() {
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
+
+    const fetchData = async () => {
+      // getUserProfile returns a promise so use try first
+      try {
+        const { data } = await getCurrentUserProfile();
+        setProfile(data);
+
+        console.log(data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
@@ -18,6 +33,14 @@ function App() {
           <>
             <h1>Logged In!</h1>
             <button onClick={logout}>Log Out</button>
+
+            {profile && (
+              <div>
+                <h1>Hi {profile.display_name}</h1>
+                <p>Followers: {profile.followers.total}</p>
+                <img src={profile.images[0].url} alt="Avatar"></img>
+              </div>
+            )}
           </>
         )}
       </header>
--- client/src/spotify/spotify.js
+++ client/src/spotify/spotify.js
@@ -126,3 +126,20 @@ const getAccessToken = () => {
 };
 
 export const accessToken = getAccessToken();
+
+
+/**
+ * Axios global request headers
+ * https://github.com/axios/axios#global-axios-defaults
+ * https://developer.spotify.com/documentation/web-api/reference/#/
+ */
+axios.defaults.baseURL = 'https://api.spotify.com/v1';
+axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+axios.defaults.headers['Content-Type'] = 'application/json';
+
+/**
+ * Get Current User's Profile
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-current-users-profile
+ * since baseURL is set, this is equivalent to https://api.spotify.com/v1/me
+ */
+export const getCurrentUserProfile = () => axios.get('/me');
